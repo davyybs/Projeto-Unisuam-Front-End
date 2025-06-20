58f1e616debde2eb20444c88056941a92cdb33e8
davyybs <daviferreira828@gmail.com>
2025-06-20 18:53:34 -0300

    Dark Mode e Ajuste no CSS

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,19 @@
-const botaoTema = document.getElementById('darkmode');
+// Aplica o modo salvo quando carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+  aplicarModoSalvo();
 
-botaoTema.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+  const botaoTema = document.getElementById("darkmode");
+  botaoTema.addEventListener("click", alternarModoEscuro);
 });
+
+// Função que vai aplicar o modo salvo
+function aplicarModoSalvo() {
+  const modoEscuroAtivo = localStorage.getItem("modoEscuro") === "true";
+  document.body.classList.toggle("dark-mode", modoEscuroAtivo);
+}
+
+// Função para alternar o modo e salvar no localStorage
+function alternarModoEscuro() {
+  const ativado = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("modoEscuro", ativado);
+}
