856bb1e16779ded54f2d1292e99f1be72559d1ce
davyybs <daviferreira828@gmail.com>
2025-06-20 19:17:21 -0300

    Ajustes

--- js/font.js
+++ js/font.js
@@ -1,22 +1,22 @@
 let tamanhoFonteAtual = 16;
 
-  function alterarFonte(delta) {
-    tamanhoFonteAtual += delta;
-    if (tamanhoFonteAtual < 10) tamanhoFonteAtual = 10;
-    if (tamanhoFonteAtual > 30) tamanhoFonteAtual = 30;
-    document.body.style.fontSize = tamanhoFonteAtual + "px";
-  }
+function alterarFonte(delta) {
+  tamanhoFonteAtual += delta;
+  if (tamanhoFonteAtual < 10) tamanhoFonteAtual = 10;
+  if (tamanhoFonteAtual > 30) tamanhoFonteAtual = 30;
+  document.body.style.fontSize = tamanhoFonteAtual + "px";
+}
 
-  function resetarFonte() {
-    tamanhoFonteAtual = 16;
-    document.body.style.fontSize = tamanhoFonteAtual + "px";
-  }
+function resetarFonte() {
+  tamanhoFonteAtual = 16;
+  document.body.style.fontSize = tamanhoFonteAtual + "px";
+}
 
-  function toggleControles() {
-    const controles = document.getElementById('mnr-fonte');
-    if (controles.style.display === 'none') {
-      controles.style.display = 'block';
-    } else {
-      controles.style.display = 'none';
-    }
+function toggleControles() {
+  const controles = document.getElementById("mnr-fonte");
+  if (controles.style.display === "none") {
+    controles.style.display = "block";
+  } else {
+    controles.style.display = "none";
   }
+}
--- js/menu_mobile.js
+++ js/menu_mobile.js
@@ -1,12 +1,16 @@
-let btnMenu = document.getElementById('btn-menu');
-let menuMobile = document.getElementById('menu-mobile');
-let fechar = document.getElementById('fechar')
+let btnMenu = document.getElementById("btn-menu");
+let menuMobile = document.getElementById("menu-mobile");
+let fechar = document.getElementById("fechar");
+let overlay = document.getElementById("overlay-menu");
 
-btnMenu.addEventListener('click', ()=> {
-    menuMobile.classList.add('abrir-menu')
-})
+btnMenu.addEventListener("click", () => {
+  menuMobile.classList.add("abrir-menu");
+});
 
-fechar.addEventListener('click', ()=> {
-    menuMobile.classList.remove('abrir-menu')
-})
+fechar.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
 
+overlay.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
