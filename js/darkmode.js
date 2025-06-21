8fbd5b94dfc7e932eac34221750eec42afa88cd0
davyybs <daviferreira828@gmail.com>
2025-06-20 22:44:10 -0300

    Atualizações

--- js/darkmode.js
+++ js/darkmode.js
@@ -17,3 +17,30 @@ function alternarModoEscuro() {
   const ativado = document.body.classList.toggle("dark-mode");
   localStorage.setItem("modoEscuro", ativado);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  aplicarModoSalvo();
+
+  const botaoTema = document.getElementById("darkmode");
+  botaoTema.addEventListener("click", alternarModoEscuro);
+});
+function aplicarModoSalvo() {
+  const modoEscuroAtivo = localStorage.getItem("modoEscuro") === "true";
+  document.body.classList.toggle("dark-mode", modoEscuroAtivo);
+  trocarImagem(modoEscuroAtivo);
+}
+function alternarModoEscuro() {
+  const ativado = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("modoEscuro", ativado);
+  trocarImagem(ativado);
+}
+function trocarImagem(escuro) {
+  const imagem = document.getElementById('imagemModo');
+  if (imagem) {
+    if (escuro) {
+      imagem.src = 'imagens/smscelularesb.png';
+    } else {
+      imagem.src = 'imagens/smscelulares.png'; 
+    }
+  }
+}
